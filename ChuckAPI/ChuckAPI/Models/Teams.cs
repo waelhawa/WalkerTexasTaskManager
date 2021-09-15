33c34e8ad0f82981f80139cd13879d35bb7b2414
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-09-15 13:50:42 -0400

    Created models and controllers for: Teams and Users.  I am getting an error.

---
+++ ChuckAPI/ChuckAPI/Models/Teams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChuckAPI.Models
+{
+    public class Teams
+    {
+      public int teamId { get; set; }
+      public int teamPoints { get; set; }
+      public string teamName { get; set; }
+    }
+}
