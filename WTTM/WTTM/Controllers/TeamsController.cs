492bc7dd08999858b08a6e4bae99e9ffd84915b0
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-09-15 20:07:09 -0400

    TeamsController Almost done

--- WTTM/WTTM/Controllers/TeamsController.cs
+++ WTTM/WTTM/Controllers/TeamsController.cs
@@ -13,10 +13,12 @@ namespace WTTM.Controllers
     [ApiController]
     public class TeamsController : ControllerBase
     {
+        //private readonly WTTM_DBContext _context;
+        private readonly ChuckDAL chuck = new ChuckDAL();
+       
         private HttpClient GetHttpClient()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.chucknorris.io/");
             return client;
         }
 
@@ -27,8 +29,30 @@ namespace WTTM.Controllers
 
             var client = GetHttpClient();
             var request = await client.GetAsync($"/get/teams");
-            var response = await request.Content.ReadAsAsync<Teams>();
-            return response;//missing syntax for grabbing a list of teams
+            var response = await request.Content.ReadAsAsync<List<Teams>>();
+            return response;
         }
+
+        //TO DO: Make new team?
+
+        //TO DO: Update team name
+        [HttpPut("{id}")] //api endpoint
+        public async Task<ActionResult> UpDateTeamName(int id, Teams teams) 
+        {
+            if(ModelState.IsValid || id != teams.TeamId) 
+            {
+                return BadRequest();
+            }
+            else 
+            {
+                Teams dbTeams =
+                //replace the values of the field here
+
+                    await
+            }
+
+        }
+
+        //TO DO: Update team points
     }
 }
