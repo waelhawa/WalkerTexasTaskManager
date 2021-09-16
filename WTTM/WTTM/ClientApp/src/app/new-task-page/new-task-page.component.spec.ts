b1f9e5b09a06f8de3e5c8f110c4fc274e101451e
Will Schoenle <wschoenle93@gmail.com>
2021-09-15 22:04:54 -0400

    major changes to new task page

--- WTTM/WTTM/ClientApp/src/app/new-task-page/new-task-page.component.spec.ts
+++ WTTM/WTTM/ClientApp/src/app/new-task-page/new-task-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NewTaskPageComponent } from './new-task-page.component';
 
 describe('NewTaskPageComponent', () => {
