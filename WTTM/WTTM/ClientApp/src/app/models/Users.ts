da01635d02bcf72b60a8c6ce954f38e95054c0a9
waelhawa <wael.hawa86@gmail.com>
2021-09-16 21:24:37 -0400

    Users model and service

---
+++ WTTM/WTTM/ClientApp/src/app/models/Users.ts
@@ -0,0 +1,7 @@
+export interface Users {
+    Id: string;
+    UserName: string;
+    Email: string;
+    PhoneNumber: string;
+    TeamId: number;
+}
---
+++ WTTM/WTTM/ClientApp/src/app/services/users.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Users } from '../models/Users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  apiUri: string = "";
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUri: string) {
+    this.apiUri = `${baseUri}/api/Users`;
+   }
+
+   //Read
+   getUsers() {
+      return this.http.get<Users[]>(`${this.apiUri}/getusers`);
+   }
+
+   getUsersById(id: string){
+    return this.http.get<Users>(`${this.apiUri}/getusersbyid/${id}`);
+   }
+
+   getUsersByUserName(userName: string){
+    return this.http.get<Users>(`${this.apiUri}/getusersbyusername/${userName}`);
+   }
+
+   getUsersByEmail(email: string){
+    return this.http.get<Users>(`${this.apiUri}/getusersbyemail/${email}`);
+   }
+
+   getUsersByPhoneNumber(phoneNumber: string){
+    return this.http.get<Users>(`${this.apiUri}/getusersbyphonenumber/${phoneNumber}`);
+   }
+
+   getUsersByTeamId(id: number){
+    return this.http.get<Users[]>(`${this.apiUri}/getusersbyid/${id}`);
+   }
+
+   //Create
+   createUser(user: Users){
+    return this.http.post<Users>(`${this.apiUri}/createuser`, user);
+   }
+
+   //Delete
+   deleteUser(id: string){
+    return this.http.delete<Users>(`${this.apiUri}/deleteuser/${id}`);
+   }
+
+   //Update
+   updateUser(id: string, user: Users){
+     return this.http.put<Users>(`${this.apiUri}/updateuser/${id}`, user);
+   }
+}
