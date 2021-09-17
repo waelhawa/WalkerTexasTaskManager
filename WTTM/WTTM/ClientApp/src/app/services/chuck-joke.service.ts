56c94c6c0622de518eb5997e70322a281c1f2a45
waelhawa <wael.hawa86@gmail.com>
2021-09-16 21:35:09 -0400

    Chuck model and service

--- WTTM/WTTM/ClientApp/src/app/services/chuck-joke.service.ts
+++ WTTM/WTTM/ClientApp/src/app/services/chuck-joke.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { ChuckJoke } from '../models/ChuckJoke';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChuckJokeService {
 
-  constructor() { }
+  apiUri: string = "";
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUri: string) {
+    this.apiUri = `${baseUri}/api/chuck`;
+   }
+
+   getRandomJoke(){
+      return this.http.get<ChuckJoke>(`${this.apiUri}/getrandomjoke`);
+   }
+
+   getJoke(category: string){
+      return this.http.get<ChuckJoke>(`${this.apiUri}/getjoke/${category}`);
+   }
 }
