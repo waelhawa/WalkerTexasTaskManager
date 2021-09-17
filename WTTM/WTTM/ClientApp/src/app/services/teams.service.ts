60eb8e49c32e327f037b94719d4189996b8d17ae
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-09-16 21:51:40 -0400

    Fixed API end points in Teams Controller

--- WTTM/WTTM/ClientApp/src/app/services/teams.service.ts
+++ WTTM/WTTM/ClientApp/src/app/services/teams.service.ts
@@ -7,38 +7,40 @@ import { Teams } from '../models/Teams';
 @Injectable({
   providedIn: 'root'
 })
+
 export class TeamsService {
   apiUri:string = "";
 
-  constructor(private http: HttpClient, @Inject('BASE_URL')baseUri:string) 
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUri:string) 
   { 
-    this.apiUri = '${baseUri}api/chuck'
-    console.log(this.apiUri)
+    this.apiUri = `${baseUri}/api/teams`;
+
+    
   }
 
-  getAllTeams(){
-    return this.http.get<Teams[]>(this.apiUri);
-  }
-
-  upDateTeams(id:number, teams:Teams)
-  {
-    return this.http.put<Teams>('${this.apiUri}/${id}', {"TeamName":teams.TeamName, "TeamPoints":teams.TeamPoints})
-  }
-
-  createNewTeam(teams: Teams)
-  {
-    return this.http.post<Teams>('${this.apiUri}',{"TeamName":teams.TeamName, "TeamPoints":teams.TeamPoints});
+  getTeams(){
+    return this.http.get<Teams[]>(`${this.apiUri}/getteams`);
   }
 
   getTeamById(id:number)
   {
-    return this.http.get<Teams>('${this.apiUri}/${id}');
+    return this.http.get<Teams>(`${this.apiUri}/getteamsbyid/${id}`);
   }
-
+  
+  createNewTeam(teams: Teams)
+  {
+    return this.http.post<Teams>(`${this.apiUri}/createnewteam`,teams);
+  }
+  
+  
   deleteTeam(id:number)
   {
-    return this.http.delete('${this.apiUri}/${id}');
+    return this.http.delete(`${this.apiUri}/deleteteam/${id}`);
   }
-
-
+  
+  upDateTeams(id:number, teams:Teams)
+  {
+    return this.http.put<Teams>(`${this.apiUri}/updateteam/${id}`, teams)
+  }
+  
 }
