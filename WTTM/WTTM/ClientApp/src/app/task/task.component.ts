edda2aaf0d2e31b6d8b5be570a71c6d2edfd1962
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-09-22 20:47:20 -0400

    Finished task.component, no obvious syntax error. Code my need review

--- WTTM/WTTM/ClientApp/src/app/task/task.component.ts
+++ WTTM/WTTM/ClientApp/src/app/task/task.component.ts
@@ -2,36 +2,47 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../models/Task';
 import { TaskService } from '../services/task.service';
 
+
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css'],
-  providers: [TaskService]
+  providers: [TaskService],
+  
 })
 export class TaskComponent implements OnInit {
-  @Input() task: Task; 
-  // {
-  //   TaskId: 1000, 
-  //   SprintId: 1000, 
-  //   UserId: 'MainUser', 
-  //   DateCreated: new Date(),
-  //   ShortDesc: 'Short Description', 
-  //   FullDesc: 'Long Description', 
-  //   StoryPoint: 1000, 
-  //   IsCompleted: false, 
-  //   TaskStatus: 'new',
-  //   DateCompleted: new Date() 
-  // };
+
+  allUATasksInSprint: Task[] = [];
+  allTasksInSprint: Task[] = [];
+
+  taskId: number;
+  sprintId: number;
+  userId: string;
+  dateCreated: Date;
+  shortDesc: string;
+  fullDesc: string;
+  storyPoint: number;
+  isCompleted: boolean;
+  taskStatus: string;
+  dateCompleted: Date;
+
+
 
   constructor(private taskServ: TaskService) { }
-  //Don't want to change too much, looks like 
+  
 
   ngOnInit(): void {
-      console.log(this.task.fullDesc)
+    this.getUassignedTasksInSprint();
+        
+    }
+
+  getUassignedTasksInSprint(){
+    this.taskServ.getUnassignedTasksInSprint(this.sprintId).subscribe(result => this.allUATasksInSprint = result)
   }
 
   //get a list of unassigned tasks withing the sprint
 
   //make sure that the user ID is null
+  //if userId == false, 
 
 }
