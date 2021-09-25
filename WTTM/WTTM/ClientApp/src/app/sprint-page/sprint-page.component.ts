e76e5e6ea5cfaa3cf2aa9c17c4599fa49d156cba
Will Schoenle <wschoenle93@gmail.com>
2021-09-25 15:11:59 -0400

    finished task component

--- WTTM/WTTM/ClientApp/src/app/sprint-page/sprint-page.component.ts
+++ WTTM/WTTM/ClientApp/src/app/sprint-page/sprint-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/Task';
+
 
 @Component({
   selector: 'app-sprint-page',
   templateUrl: './sprint-page.component.html',
-  styleUrls: ['./sprint-page.component.css']
+  styleUrls: ['./sprint-page.component.css'],
+  providers: [TaskService]
 })
 export class SprintPageComponent implements OnInit {
+empty: boolean = false;
+chuckGif: string ="/assets/images/Chuck Gif.gif";
+allTasks: Task[]=[];
 
-  constructor() { }
+  constructor(private taskServ: TaskService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getTasksBySprintId();
+
+  }
+
+  getTasksBySprintId(){
+    this.taskServ.getUnassignedTasksInSprint
   }
 
 }
--- WTTM/WTTM/ClientApp/src/app/sprint/sprint.component.ts
+++ WTTM/WTTM/ClientApp/src/app/sprint/sprint.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Sprints } from '../models/Sprints';
-import { Task } from '../models/Task';
 import { Teams } from '../models/Teams';
 import { Users } from '../models/Users';
 import { SprintService } from '../services/sprint.service';
@@ -17,41 +16,13 @@ import { UsersService } from '../services/users.service';
 
 })
 export class SprintComponent implements OnInit {
+  @Input() sprint: Sprints;
 
-  tasksNotAssigned: Task[] = [];
-  allSprints: Sprints[] = [];
-  currentSprint: Sprints;
-  currentTeam: Teams;
-  currentUser: Users;
-
-  constructor( private sprintServe: SprintService, private taskserv: TaskService, private teamServ:TeamsService, private userServ: UsersService) { }
+  constructor( ) { }
 
   ngOnInit(): void {
-    
-   this.getSprints();
 
   }
 
-  getSprints(){
-    this.sprintServe.getSprints().subscribe(
-      result => this.allSprints = result
-    )
-  }
-
-  // getUnassignedTasksInSprint(){
-  //   this.taskserv.getUnassignedTasksInSprint(this.currentSprint.sprintId).subscribe(
-  //     result => this.tasksNotAssigned = result
-  //   )
-  // }
-
-  // getSprintByTeamId(){
-  //   this.sprintServe.getSprintsByTeamId(this.currentTeam.teamId).subscribe(
-  //     result => this.currentSprint = result
-  //   )
-  // }
-
-  // getTeamByUserId(){
-
-  // }
 
 }
