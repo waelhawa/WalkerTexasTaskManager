97f0c918833f2b20a53226e60d9f5514bc81db59
Will Schoenle <wschoenle93@gmail.com>
2021-09-26 19:06:05 -0400

    routing

--- WTTM/WTTM/ClientApp/src/app/app.routing.ts
+++ WTTM/WTTM/ClientApp/src/app/app.routing.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from "./home/home.component";
 import { NewTaskPageComponent } from "./new-task-page/new-task-page.component";
 import { SprintDetailsComponent } from "./sprint-details/sprint-details.component";
 import { SprintComponent } from "./sprint/sprint.component";
+import { NewSprintComponent } from "./new-sprint/new-sprint.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: '/new-task-page', component: NewTaskPageComponent},
   {path: '/backlog', component: BacklogComponent},
   {path: '/sprint', component: SprintComponent},
+  {path: '/new-sprint', component: NewSprintComponent},
 
 
   {path: '**', redirectTo: ''}
