0c550048984875575c4c58ef9394c8240c7bcf4e
waelhawa <wael.hawa86@gmail.com>
2021-09-27 21:30:51 -0400

    Update sprint page

--- WTTM/WTTM/ClientApp/src/app/home/home.component.ts
+++ WTTM/WTTM/ClientApp/src/app/home/home.component.ts
@@ -80,7 +80,7 @@ export class HomeComponent implements OnInit {
 
   displayTeamName(){
     if (this.partOfTeam){
-
+      
     }
   }
 
--- WTTM/WTTM/ClientApp/src/app/sprint/sprint.component.ts
+++ WTTM/WTTM/ClientApp/src/app/sprint/sprint.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { get } from 'http';
 import { Sprints } from '../models/Sprints';
+import { Task } from '../models/Task';
 import { Teams } from '../models/Teams';
 import { Users } from '../models/Users';
 import { SprintService } from '../services/sprint.service';
@@ -17,16 +19,27 @@ import { UsersService } from '../services/users.service';
 })
 export class SprintComponent implements OnInit {
   @Input() sprint: Sprints;
+  totalPoints: number = 0;
 
-  constructor(private sprintServ: SprintService ) { }
+  constructor(private sprintServ: SprintService, private taskServ: TaskService) { }
 
   ngOnInit(): void {
+    this.getPointCount();
 
   }
 
-deleteSprint(id: number){
-  this.sprintServ.deleteSprint(id).subscribe();
-  window.location.reload();
-}
+  deleteSprint(id: number) {
+    this.sprintServ.deleteSprint(id).subscribe();
+    window.location.reload();
+  }
+
+  getPointCount(){
+    this.taskServ.getTasksBySprintId(this.sprint.sprintId).subscribe(result => {
+      result.forEach(element => {
+        this.totalPoints += element.storyPoint;
+      });
+      this.sprint.totalStoryPoint = this.totalPoints;
+    });
+  }
 
 }
